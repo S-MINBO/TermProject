72dbc9904c3cfbb6ac8328dd1c68dbd05da41e72
S-Minbo <smb5174@gmail.com>
2022-12-17 18:32:25 +0900

    5

---
+++ opencv-hand-tracking-unity/README.md
@@ -0,0 +1,5 @@
+# Hand Tracking in OpenCV and transfering points to Unity
+
+Based on Youtube tutorial: https://www.youtube.com/watch?v=RQ-2JWzNc6k&t=21s
+
+<img src="https://github.com/KovalevCG/opencv-hand-tracking-unity/blob/master/gifs/hand-in-vr_02.gif" width="850">
---
+++ opencv-hand-tracking-unity/unity/3D Hand Tracking/Assets/HandTracking.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandTracking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public UDPReceive UDPReceive;
+    public GameObject[] handPoints;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string data = UDPReceive.data;
+        data = data.Remove(0, 1);
+        data = data.Remove(data.Length-1, 1);
+        print(data);
+        string[] points = data.Split(',');
+        print(points[0]);
+        for (int i = 0; i < 21; i++)
+        {
+            float x = (float.Parse(points[i * 3]) - 640)/70;
+            float y = (float.Parse(points[i * 3 + 1])) / 70;
+            float z = float.Parse(points[i * 3 + 2]) / 70;
+
+            handPoints[i].transform.localPosition = new Vector3(x, y, z);
+        }
+    }
+}
